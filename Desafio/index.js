2a03f2eb0e95f15abd7ecf3db4672ae96461cb88
limarodolfo <rdlflima@gmail.com>
2024-08-24 12:27:16 -0300

    início do projeto

--- Desafio/index.js
+++ Desafio/index.js
@@ -1,39 +1,12 @@
-let nome = ""
-let nivelXp = 0
+let heroName = "Chuck Norris "
 
-nome = prompt("Digite seu nome?")
-nivelXp = prompt("Qual é o seu nível?")
+let XPlevel = 0
 
-let nivel;
 
-switch (true){
-    case nivelXp === 1000:
-        nivel = "Seu nível é Ferro";
-    break;
 
-    case nivelXp >= 1001 && nivelXp <=  2000:
-        nivel = "Seu nível é Bronze";
-    break;
+const messageOfHero = "O herói de nome "
 
-    case nivelXp >=  2001 && nivelXp <= 5000:
-        nivel = "Seu nível é Prata";
-    break;
+const messageOfXP = "está no nível "
 
-    case nivelXp >= 5001 && nivelXp <= 7000:
-        nivel = "Seu nível é ouro";
-    break;
+console.log(messageOfHero + heroName + messageOfXP + XPlevel )
 
-    case nivelXp >= 7001 && nivelXp <= 8000:
-        nivel = "Seu nível é Platina";
-    break;
-    
-    case nivelXp >= 8001 && nivelXp <= 9000:
-        nivel = "Seu nível é Ascendente";
-    break;
-
-    case nivelXp >= 10001:
-        nivel ="Seu nível é Imortal";
-    break;
-}
-
-console.log("O Herói de nome "+ nome + " está no nível de " + nivel + " com " +nivelXp + "Xp.")
