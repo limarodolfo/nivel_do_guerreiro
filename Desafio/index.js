2b3bbfc79f9fa8a096001e04e0386259bb6a070a
limarodolfo <rdlflima@gmail.com>
2024-09-12 22:58:53 -0300

    atualizacao_6

--- Desafio/index.js
+++ Desafio/index.js
@@ -1,42 +1,23 @@
-let heroName = "Chuck Norris "
+let nome = "Rodolfo Lima"
+let xp = 9000
+let nivel = ""
 
-for (let XPlevel = 0; XPlevel <= 10001; XPlevel++){
-
-    console.log (XPlevel)
-
-    switch (true){
-        case XPlevel <= 1000:
-            console.log("O herói de nome " + heroName + "está no nível de ferro")
-            break
-        
-            case XPlevel >= 1001 && XPlevel <= 2000:
-            console.log("O herói de nome " + heroName + "está no nível de bronze")
-            break
-        
-            case XPlevel >= 2001 && XPlevel <= 5000:
-            console.log("O herói de nome " + heroName + "está no nível de prata")
-            break
-        
-            case XPlevel >= 5001 && XPlevel <= 7000:
-            console.log("O herói de nome " + heroName + "está no nível de ouro")
-            break
-        
-            case XPlevel >= 7001 && XPlevel <= 8000:
-            console.log("O herói de nome " + heroName + "está no nível de platina")
-            break
-        
-            case XPlevel >= 8001 && XPlevel <= 9000:
-            console.log("O herói de nome " + heroName + "está no nível de ascendente")
-            break
-        
-            case XPlevel >= 9001 && XPlevel <= 10000:
-            console.log("O herói de nome " + heroName + "está no nível de imortal")
-            break
-        
-            case XPlevel >= 10001:
-            console.log("O herói de nome " + heroName + "está no nível de radiante")
-            break
-        }
-    
+if (xp <= 1000) {
+    nivel = "Ferro"
+} else if (xp <= 2000) {
+    nivel = "Bronze"
+} else if (xp <= 5000) {
+    nivel = "Prata"
+} else if (xp <= 7000) {
+    nivel = "Ouro"
+} else if (xp <= 8000) {
+    nivel = "Platina"
+} else if (xp <= 9000) {
+    nivel = "Ascendente"
+} else if (xp <= 10000) {
+    nivel = "Imortal"
+} else if (xp <= 10001) {
+    nivel = "Radiante"
 }
 
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
